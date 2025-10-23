b3819094542247ae572156e467c473c52288430b
María Leonor Rosales <male.rosales1205@gmail.com>
2025-10-23 11:38:59 -0600

    MAs camvios para web

--- app.py
+++ app.py
@@ -39,5 +39,9 @@ def index():
         texto=texto
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
